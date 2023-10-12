10d1915ec8b1355b4602ddf4aa0729f946e5f528
yen <l.yennie.j@gmail.com>
2023-10-12 16:01:10 +0900

    90's test start

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>90's fighting!</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
